e6381139c6e67f135818c9b9d9a4f0c7957aa85b
Thambidurai-Zoho <95858315+Thambidurai-Zoho@users.noreply.github.com>
2022-01-04 10:18:02 +0530

    Update sharedLetters.cpp

--- sharedLetters.cpp
+++ sharedLetters.cpp
@@ -18,14 +18,14 @@ N/A
 int sharedLetters(std::string str1, std::string str2) {
 	  int sharedLettersCount = 0;
 	   for(int i = 0 ; i < str1.length() ; i++){
-			     for(int j = 0; j < str2.length(); j++){
-						    if(str1[i]==str2[j]){// if str1 letter and str2 letter are same we increase the sharedLettersCount .
-									  sharedLettersCount++;
-									  str2[j]='>';//sometimes the same letter repeated multi times its also count in the sharedLettersCount.its a wrong way of counting the sharedLettersCount so we avoid this count we change the second string character to '<'. 
-									  break;
+	        for(int j = 0; j < str2.length(); j++){
+		     if(str1[i]==str2[j]){// if str1 letter and str2 letter are same we increase the sharedLettersCount .
+		     sharedLettersCount++;
+		     str2[j]='>';//sometimes the same letter repeated multi times its also count in the sharedLettersCount.its a wrong way of counting the sharedLettersCount so we avoid this count we change the second string character to '<'. 
+		     break;
 								}
-					 }
 		 }
+       	 }
 	  return sharedLettersCount; 
 	
 }
