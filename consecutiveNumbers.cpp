d23e05234176caea0f839968c9f02ab2be7a2dcd
Thambidurai-Zoho <95858315+Thambidurai-Zoho@users.noreply.github.com>
2022-01-04 10:23:55 +0530

    Create consecutiveNumbers.cpp

---
+++ consecutiveNumbers.cpp
@@ -0,0 +1,30 @@
+/*
+Create a function that determines whether elements in an array can be re-arranged to form a consecutive list of numbers where each number appears exactly once.
+
+Examples
+cons([5, 1, 4, 3, 2]) ➞ true
+// Can be re-arranged to form [1, 2, 3, 4, 5]
+
+cons([5, 1, 4, 3, 2, 8]) ➞ false
+
+cons([5, 6, 7, 8, 9, 9]) ➞ false
+// 9 appears twice 
+Notes
+N/A
+
+*/
+
+
+
+
+
+
+bool cons(std::vector<int> arr) {
+	   sort(arr.begin(),arr.end());
+	   for(int i = 0 ; i < arr.size() - 1 ; i++){
+			    if(arr[i] + 1 != arr[i+1]){
+						 return false;
+					}    
+		 }
+	  return true;
+}
