b6c17c756c58206c1fb3517a05bc78a9960e60ca
Thambidurai-Zoho <95858315+Thambidurai-Zoho@users.noreply.github.com>
2022-01-05 12:45:32 +0530

    Update isCorrectAliases.cpp

--- isCorrectAliases.cpp
+++ isCorrectAliases.cpp
@@ -18,19 +18,19 @@ using namespace std;
 
 bool isCorrectAliases(vector<string> names, vector<string> aliases) {
 	  for(int i=0;i< names.size() ; i++){
-			   int flag=1;
-         //we check the first word of the first letter is same like as a names first letter
-			   if(names[i][0]==aliases[i][0]){
-					   for(int j=1;j < aliases[i].size();  j++){
-							   if(aliases[i][j]==' '){
-									   j++;
-                   //we check the second word of the first letter is same like as a names first letter
-									   if(aliases[i][0]==aliases[i][j]){
-											   flag=0;
-										 }
-								 }
-						 }
+	        int flag=1;
+                //we check the first word of the first letter is same like as a names first letter
+	        if(names[i][0]==aliases[i][0]){
+		     for(int j=1;j < aliases[i].size();  j++){
+			  if(aliases[i][j]==' '){
+			       j++;
+                               //we check the second word of the first letter is same like as a names first letter
+			       if(aliases[i][0]==aliases[i][j]){
+				    flag=0;
 				 }
+			 }
+		 }
+	 }
 			 if(flag==1){
 				  return false;
 			 }
